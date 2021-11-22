83152dc48bac17c118d54439de90ecd37419e04a
als95 <als950901@gmail.com>
2021-11-22 21:26:46 +0900

    init

---
+++ src/test/kotlin/com/mediaservice/MediaServiceBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mediaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediaServiceBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
