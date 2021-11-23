bcc9644b1406cd8ef8224b77b6d8ec40a5534f90
qowlsdn8007 <qowlsdn8007@gmail.com>
2021-11-23 22:39:45 +0900

    test commit

--- src/test/kotlin/com/mediaservice/MediaServiceBackendApplicationTests.kt
+++ src/test/kotlin/com/mediaservice/MediaServiceBackendApplicationTests.kt
@@ -3,6 +3,10 @@ package com.mediaservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+// for the test commit
+
+
+
 @SpringBootTest
 class MediaServiceBackendApplicationTests {
 
