c81d724c5852371a9db65966815f7f58a1e362c6
jinwoo <qowlsdn8007@gmail.com>
2021-11-24 14:43:47 +0900

    chore: CI init with github actions

--- src/test/kotlin/com/mediaservice/MediaServiceBackendApplicationTests.kt
+++ src/test/kotlin/com/mediaservice/MediaServiceBackendApplicationTests.kt
@@ -3,10 +3,6 @@ package com.mediaservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-// for the test commit
-
-
-
 @SpringBootTest
 class MediaServiceBackendApplicationTests {
 }
